ce1b83a426febab66c12279c98a916b4f77a3bee
Belov Pavel <belovpavel@protonmail.com>
2019-06-12 20:54:11 +0300

    task-1

---
+++ src/modules/copyFile.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const { config } = require("../index");
+
+const copyFile = (base, item) => {
+  let outputDirPath = path.join(__dirname, "../../", config.output, item[0]);
+
+  if (!fs.existsSync(outputDirPath)) {
+    fs.mkdirSync(outputDirPath);
+  }
+
+  fs.copyFileSync(
+    path.join(__dirname, "../../", base, item),
+    path.join(outputDirPath, item)
+  );
+  console.log(`Файл ${item} скопирован!`);
+
+  if (config.delete) {
+    fs.unlinkSync(path.join(__dirname, "../../", base, item));
+    console.log(`Исходный файл ${item} удален!`);
+  }
+};
+
+module.exports = copyFile;
